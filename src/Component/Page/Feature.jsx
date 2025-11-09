658dfdb77e771dd770975a79532240446812ed94
rakibulzain <rakibulzain@gmail.com>
2025-11-09 22:37:48 +0600

    Remove Button

--- src/Component/Page/Feature.jsx
+++ src/Component/Page/Feature.jsx
@@ -5,12 +5,10 @@ export default function Feature() {
   return (
     <div className='bg-[#e8ebe2] border border-b-black'>
         <div className="gap-5 flex flex-col">
-            <h1 className='text-black flex justify-center items-center text-3xl mt-5  underline decoration-black decoration-2 underline-offset-4'>Feature Products</h1>
-            <div className='flex gap-5 ml-5'>
-              <button className='btn px-5 py-1'>Mens</button>
-              <button className='btn px-5 py-1'>Women</button>
-            </div>
+            <h1 className='text-black flex justify-center items-center text-3xl mt-5  underline decoration-black decoration-2 underline-offset-4'>Feature Products</h1>     
         </div>
+
+
         <div className="grid grid-cols-1 gap-3 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 mt-10 ">
           <Card title='Nike' price='3500' model='This is most selling products' image="src/assets/background-remover/png-clipart-nike-free-air-force-nike-air-max-sneakers-men-shoes-white-outdoor-shoe.png" mainprice = '3800'></Card>
           <Card title= 'Puma' price= '4220' model='RS-X Hyper Prep Fall' image='src/assets/background-remover/running-shoes-sneakers-transparent-background_84443-4651.png' mainprice='4880' ></Card>
