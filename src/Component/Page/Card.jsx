00d054c9dc82cc51e85276bb39e73a5a4bb493a5
rakibulzain <rakibulzain@gmail.com>
2025-11-11 02:20:07 +0600

    add json details on card item

--- src/Component/Page/Card.jsx
+++ src/Component/Page/Card.jsx
@@ -1,24 +1,27 @@
+import React from "react";
 
-export default function Card({ title, price, model,image,mainprice }) {
+export default function Card({ name, price, brand, imageURL }) {
   return (
-    <div className="card-body bg-white w-full max-w-sm mx-auto shadow-lg rounded-lg p-6 mb-5">
-      <div className="">
-        <div className="bg-zinc-300 shadow-lg rounded-lg overflow-hidden ">
-          <img className="w-full h-60 object-cover" src={image} alt="Shoes" />
-        </div>
-        <div className="mt-5">
-          <h2 className="card-title text-3xl text-black">{title}</h2>
-          <p className="text-black">{model}</p>
-          <p className="text-black font-bold text-2xl mt-2">BDT {price}TK <span className="text-black/60 line-through text-sm ">BDT {mainprice}TK</span></p>
-          
-        </div>
-        <div className="flex gap-4 mt-4 justify-center items-center">
-          <button className=" bg-blue-700 px-2 p-1 rounded hover:bg-black duration-300 ">Buy Now</button>
-          <button className=" bg-blue-700 p-1 rounded px-1 hover:bg-black duration-300">Add to cart</button>
-        </div>
+    <div className="bg-white text-black w-full shadow-lg rounded-lg p-6">
+      <div className="bg-gray-400 shadow-lg rounded-lg overflow-hidden">
+        <img className="w-full h-60 object-cover" src={imageURL} alt={imageURL} />
+      </div>
+      <div className="mt-5">
+        <h2 className="text-xl font-bold">{name}</h2>
+        <p className="text-gray-700">{brand}</p>
+        <p className="font-bold text-xl mt-2">
+          $ {price}
+         
+        </p>
+      </div>
+      <div className="flex gap-4 mt-4 justify-center text-white">
+        <button className="bg-blue-700 px-4 py-2 rounded hover:bg-black duration-300">
+          Buy Now
+        </button>
+        <button className="bg-blue-700 px-4 py-2 rounded hover:bg-black duration-300">
+          Add to Cart
+        </button>
       </div>
     </div>
-    
-  
-  )
+  );
 }
--- src/Component/Page/Feature.jsx
+++ src/Component/Page/Feature.jsx
@@ -1,29 +1,47 @@
-import { CiStar } from "react-icons/ci";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 
 export default function Feature() {
-  return (
-    <div className='bg-[#e8ebe2] border border-b-black'>
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/shoesdata.json") // public folder থেকে JSON load
+      .then((res) => res.json())
+      .then((data) => {
+        const productsArray = Object.values(data); // object → array
+        setProducts(productsArray);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (products.length === 0)
+    return <p className="text-center mt-10">Loading...</p>;
+
+    return (
+      <div className='bg-[#e8ebe2] border border-b-black'>
         <div className="flex justify-center items-center mt-5 ">
-            <h1 className="text-3xl text-black font-semibold underline underline-offset-8 ">Frature <span className="text-blue-500">Products</span></h1>     
+          <h1 className="text-3xl text-black font-semibold underline underline-offset-8 ">Frature <span className="text-blue-500">Products</span></h1>
         </div>
 
 
         <div className="grid grid-cols-1 gap-3 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 mt-10 ">
-          <Card title='Nike' price='3500' model='This is most selling products' image="src/assets/background-remover/png-clipart-nike-free-air-force-nike-air-max-sneakers-men-shoes-white-outdoor-shoe.png" mainprice = '3800'></Card>
-          <Card title= 'Puma' price= '4220' model='RS-X Hyper Prep Fall' image='src/assets/background-remover/running-shoes-sneakers-transparent-background_84443-4651.png' mainprice='4880' ></Card>
-          <Card title='Adidas' price='2999' model='Adidas Ultraboost 22 Shoes' image='src/assets/background-remover/pngtree-white-sports-shoe-with-a-rugged-design-png-image_15815147.png' mainprice='3500'></Card>
-          <Card title='Reebok' price='4500' model='Reebok Zig Kinetica II Shoes' image='src/assets/background-remover/white.png' mainprice='5000'></Card>
-          <Card title='New Balance' price='3800' model='New Balance Fresh Foam X 1080v12' image='src/assets/background-remover/png-transparent-nike-free-nike-air-max-shoe-white-sportswear-nike-leather-grey-outdoor-shoe-thumbnail.png' mainprice='4200'></Card>
-          <Card title='Asics' price='4100' model='Asics Gel-Kayano 28 Shoes' image='src/assets/background-remover/running-shoes-sneakers-transparent-background_84443-4651.png' mainprice='4600'></Card>
-          <Card title='Nike' price='4200' model='Sealdine RX-9 cover' image='src/assets/background-remover/imgbin-nike-air-max-sneakers-air-jordan-shoe-nike-air-ALrpR62gyGx7Km4GjVBiwSaak_t.png' mainprice='5220' ></Card>
-          <Card title='Nike' price='2800' model='Sealdine RX-6 cover' mainprice='3220' image='src/assets/background-remover/png-clipart-nike-air-max-sneakers-shoe-fashion-nike-white-fashion.png' ></Card>
+          {products.map((product) => {
+            return (
+              <Card
+                key={product.id}
+                name={product.name}
+                brand={product.brand}
+                price={product.price}
+                mainprice={product.mainprice}
+                imageURL={product.imageURL} />
+            );
+          })}
         </div>
-      
-
-       </div>
 
 
+      </div>
 
-  )
-}
+
+
+    )
+  }
