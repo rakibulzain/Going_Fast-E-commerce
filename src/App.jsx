d650ad6a8ab45fe54133e9e854f0e6f7792812c4
rakibulzain <rakibulzain@gmail.com>
2025-11-14 20:04:32 +0600

    creat new path

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Footer from './Component/Footer.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router'
 import Home from './Component/Page/Home.jsx'
 import Contact from './Component/Page/Contact.jsx'
-import Categories from './Categories.jsx'
+import Feature from './Component/Page/Feature.jsx'
+import MensShoes from './Component/Page/MensShoes.jsx'
+import WomenShoes from './Component/Page/WomenShoes.jsx'
 
 
 
@@ -15,9 +17,12 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/categories' element={<Categories/>} />
         <Route path='/contact' element={<Contact/>}/>
-        <Route path='/deatils' element={<div>this is card deatils</div>} />
+        <Route path='/all' element={<Feature/>}/>
+        <Route path='/mens-shoes' element={<MensShoes/>}/>
+        <Route path='/women-shoes' element={<WomenShoes/>}/>
+        <Route path='/kids-shoes' element={<h1>kids shoes</h1>}/>
+        
       </Routes>
       <Footer />
     </Router>
