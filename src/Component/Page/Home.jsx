bda98ee1046a123d6a82d232854b71a9ecf5490b
rakibulzain <rakibulzain@gmail.com>
2025-11-14 20:05:28 +0600

    create men and women sections

--- src/Component/Page/Home.jsx
+++ src/Component/Page/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Hero from './Hero'
-import Navbar from './Navbar'
-import Footer from '../Footer'
+
 import Feature from './Feature'
 
 export default function Home() {
---
+++ src/Component/Page/MensShoes.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import Card from './Card';
+
+export default function MensShoes() {
+
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("/shoesdata.json")
+            .then((res) => res.json())
+            .then((data) => {
+                const productsArray = Object.values(data);
+                setProducts(productsArray);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+    const menProducts = products.filter(item => item.gender === "MEN");
+    return (
+        <div className='bg-[#e8ebe2] border border-b-black'>
+            <div className="flex justify-center items-center mt-5 ">
+                <h1 className="text-3xl text-black font-semibold underline underline-offset-8 ">Mens Shoes <span className="text-blue-500">Products</span></h1>
+            </div>
+
+
+            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 mt-10 ">
+
+                {menProducts.length > 0 &&(menProducts.map(product =>(<Card key={product.id}
+                name={product.name}
+                brand={product.brand}
+                price={product.price}
+                mainprice={product.mainprice}
+                imageURL={product.imageURL}
+                discount={product.discount}
+                gender={product.gender}/>)))}
+                
+            </div>
+
+
+        </div>
+    )
+}
