04e72067f122f71f6a4e43b62c02a8aee3a87180
rakibulzain <rakibulzain@gmail.com>
2025-11-24 09:57:31 +0600

    update hero section

--- src/Component/Page/Hero.jsx
+++ src/Component/Page/Hero.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
+import React from "react";
+import heroimg from "../../assets/logoheroimg.png";
 
 export default function Hero() {
   return (
-    <div className='bg-[url("src/assets/logoheroimg.png")] h-screen bg-cover bg-black relative'>
-      <div className='text-black'>
-         <div className=' flex flex-col justify-center items-start h-screen ml-5 gap-5'>
-        <div className='gap-5 flex flex-col w-1/2'>
-           <h1 className='text-6xl font-bold'>Step Into <span className='text-blue-500'>Style</span> </h1>
-           <p>Step into ultimate style and comfort with our premium sneakers. Designed for performance, durability, and confidence — perfect for every move and every moment in your journey.
-</p>
-             <div>
-                <button className='text-white bg-black px-5 py-2 rounded hover:bg-black/70'>Explore Now</button>
-             </div>
+    <div
+      className="h-screen bg-cover bg-center relative"
+      style={{ backgroundImage: `url(${heroimg})` }}
+    >
+      <div className="flex flex-col justify-center items-start h-full ml-5 gap-5 text-black">
+        
+        <div className="gap-5 flex flex-col w-1/2">
+          
+          <h1 className="text-6xl font-bold">
+            Step Into <span className="text-blue-500">Style</span>
+          </h1>
+
+          
+          <p className="text-lg leading-relaxed">
+            Step into ultimate style and comfort with our premium sneakers. 
+            Designed for performance, durability, and confidence — perfect for 
+            every move and every moment in your journey.
+          </p>
+
+          
+          
         </div>
-       
+        <div>
+          <button className="text-white bg-black px-3 py-2  rounded hover:bg-black/70">
+            Explore Now
+          </button>
+        </div>
+
       </div>
-      </div>
-     
     </div>
-  )
+  );
 }
